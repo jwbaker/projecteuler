396ba41a2ed59d5657d47dd6e6a0093fcea0371b
Jason Baker <jason.baker@remote-learner.net>
2013-03-28 15:19:33 -0400

    Major improvements to PE17: it now counts accurately up to 999

--- PE17.py
+++ PE17.py
@@ -14,12 +14,13 @@ import math;
 
 def sumDigits(num):
     if num == 0: return 0;
-    elif num == 0: return 36;
+    elif num == 9:
+        return 36;
     
     total = 0;
-    singles = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
-    for i in range(1, num + 1):
+    for i in range(0, num):
         total += len( singles[i] );
         
     return total;
@@ -28,9 +29,9 @@ def sumTeens(num):
     if num == 9: return 67;
     
     total = 0;
-    teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     
-    for i in range(1, num + 1):
+    for i in range(0, num):
         total += len( teens[i] );
         
     return total;
@@ -42,18 +43,51 @@ def sumTens(num):
     total = 0;
     tens = int( math.floor(num/10) );
     ones = num % 10;
-    terms = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     
-    for i in range(1, tens + 1):
-        total += len( terms[i] );
+    terms = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     
+    for i in range(0, tens):
+        if i == 0:
+            total += len(terms[i]);
+        elif i == tens - 1:
+            total += (ones + 1) * len(terms[i]);
+        else:
+            total += 10 * len(terms[i]);
+            
     total += (tens - 1) * sumDigits(9);
     
     if tens == 1:
         total += sumDigits(9);
-        return sumTeens(ones);
+        return total + sumTeens(ones);
     else:
         total += sumTeens(9);
         return total + sumDigits(ones);
     
-print(sumTens(25))
+def sumHundreds(num):
+    if num < 100:
+        return sumTens(num);
+    
+    total = 0;
+    hundreds = int( math.floor(num/100) );
+    tens = num % 100;
+    
+    total += hundreds * sumTens(99);
+    total += (hundreds + tens) * sumDigits(hundreds);
+    total += hundreds * len("hundred");
+    total += tens * ( len("hundred") + len("and") );
+    
+    return total + sumTens(tens);
+
+def sumThousands(num):
+    if num < 1000:
+        return sumHundreds(num);
+    
+    
+
+def sumLettersOfNum(num):
+    if num >= 10000:
+        return "Too big";
+    else:
+        return sumThousands(num);
+
+print(sumLettersOfNum(1000));
