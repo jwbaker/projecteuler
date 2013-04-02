6918c4b093c2e90c2b82230ccd19f8293ecf21f7
Jason Baker <jason.baker@remote-learner.net>
2013-04-02 16:29:32 -0400

    Completed PE17 and started on PE18

--- PE17.py
+++ PE17.py
@@ -9,85 +9,80 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage. 
 @author: Jason Baker
 '''
-
 import math;
 
-def sumDigits(num):
-    if num == 0: return 0;
-    elif num == 9:
-        return 36;
-    
-    total = 0;
-    singles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    
-    for i in range(0, num):
-        total += len( singles[i] );
-        
-    return total;
-
-def sumTeens(num):
-    if num == 9: return 67;
-    
-    total = 0;
-    teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    
-    for i in range(0, num):
-        total += len( teens[i] );
-        
-    return total;
-
-def sumTens(num):
-    if num < 10:
-        return sumDigits(num);
-    
-    total = 0;
-    tens = int( math.floor(num/10) );
-    ones = num % 10;
-    
-    terms = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    
-    for i in range(0, tens):
-        if i == 0:
-            total += len(terms[i]);
-        elif i == tens - 1:
-            total += (ones + 1) * len(terms[i]);
-        else:
-            total += 10 * len(terms[i]);
+def toDigit(num):
+    result = "";
+    nums = ["",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty"];
+    tens = ["",
+            "ten",
+            "twenty",
+            "thirty",
+            "forty",
+            "fifty",
+            "sixty",
+            "seventy",
+            "eighty",
+            "ninety"];
             
-    total += (tens - 1) * sumDigits(9);
-    
-    if tens == 1:
-        total += sumDigits(9);
-        return total + sumTeens(ones);
+    if num <= 20:
+        result += nums[num];
     else:
-        total += sumTeens(9);
-        return total + sumDigits(ones);
-    
-def sumHundreds(num):
-    if num < 100:
-        return sumTens(num);
-    
-    total = 0;
-    hundreds = int( math.floor(num/100) );
-    tens = num % 100;
-    
-    total += hundreds * sumTens(99);
-    total += (hundreds + tens) * sumDigits(hundreds);
-    total += hundreds * len("hundred");
-    total += tens * ( len("hundred") + len("and") );
-    
-    return total + sumTens(tens);
+        result += tens[int(math.floor(num/10))];
+        if num % 10 != 0:
+            result += nums[num%10];
+            
+    return result;
 
-def sumThousands(num):
-    if num < 1000:
-        return sumHundreds(num);
+def toStringHelp(arr, num, decimalPosition):
+    posns = ["", "", "", "hundred", "thousand"];
     
-    
+    if decimalPosition == 0:
+        return "";
+    elif decimalPosition == 1:
+        return toDigit(num);
+    elif decimalPosition == 2 and arr[0] == 1:
+        return toDigit(num);
+    elif decimalPosition == 2:
+        return toDigit(num); 
+    elif num - (int(arr[0]) * 10**(decimalPosition - 1)) == 0 or num - (int(arr[0]) * 10**(decimalPosition - 1)) % 10**(decimalPosition - 2) == 0:
+        return toDigit(int(arr[0])) + posns[decimalPosition];
+    else:
+        return toDigit(int(arr[0])) + posns[decimalPosition] + "and" + toStringHelp(arr[1:], num - (int(arr[0]) * 10**(decimalPosition - 1)), decimalPosition - 1);
 
-def sumLettersOfNum(num):
+def toString(num):
+    digits = str(num);
+    return toStringHelp(digits, num, len(digits));
+    
+def lettersOfNum(num):
     if num >= 10000:
         return "Too big";
     else:
-        return sumThousands(num);
+        return toString(num);
 
-print(sumLettersOfNum(1000));
+total = 0;
+
+for i in range(1,1001):
+    total += len(lettersOfNum(i));
+    
+print(total);
