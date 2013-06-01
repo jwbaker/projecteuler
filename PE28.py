9b2098c8636ef2557ac8b7d52f2f5755e9d376e2
Jason <jwbaker@uwaterloo.ca>
2013-06-01 01:13:02 -0400

    Started PE28

---
+++ PE28.py
@@ -0,0 +1,19 @@
+'''
+Created on 2013-06-01
+
+Problem 28:
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+*21* 22 23 25 *25*
+20 *7* 8 *9* 10
+19 6 *1*  2 11
+18 *5* 4 *3* 12
+*17* 16 15 14 *13*
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals of a 1001 by 1001 spiral formed in the same way?
+
+SOLUTION: ???
+@author: Jason Baker
+'''
